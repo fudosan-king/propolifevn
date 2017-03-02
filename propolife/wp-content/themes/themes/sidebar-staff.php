ef0c9d0e350b5a21fef50508ee9afb59bd980609
Khanh Nguyen <khanh@fudosan-king.jp>
2017-03-02 11:12:18 +0700

    update staff

--- propolife/wp-content/themes/themes/sidebar-staff.php
+++ propolife/wp-content/themes/themes/sidebar-staff.php
@@ -31,21 +31,27 @@
 <div class="col-lg-4 col-md-4 col-sm-4" align="center">
 <div style="background-color:#000000;color:#ffffff"><img src="<?php bloginfo( 'template_directory' );?>/images/people-2.jpg" class="img-responsive" />
 <div style="" class="per-name">Mr. Suzuki Hideaki</div>
-<div><span class="per-pos">M</span>anager</div>
+<div>&nbsp;
+    <!-- <span class="per-pos">M</span>anager -->
+</div>
 </div>
 </div>
 
 <div class="col-lg-4 col-md-4 col-sm-4" align="center">
 <div style="background-color:#000000;color:#ffffff"><img src="<?php bloginfo( 'template_directory' );?>/images/people-1.jpg" class="img-responsive" />
-<div style="" class="per-name">Mr. Hà Thanh Trung</div>
-<div><span class="per-pos">A</span>rchitect</div>
+<div style="" class="per-name">Mr. Watanabe Masaru</div>
+<div>&nbsp;
+    <!-- <span class="per-pos">A</span>rchitect -->
+</div>
 </div>
 </div>
 
 <div class="col-lg-4 col-md-4 col-sm-4" align="center">
 <div style="background-color:#000000;color:#ffffff"><img src="<?php bloginfo( 'template_directory' );?>/images/people-3.jpg" class="img-responsive" />
 <div style="" class="per-name">Mr. Trần Chí Trung</div>
-<div><span class="per-pos">C</span>onstruction</div>
+<div>&nbsp;
+    <!-- <span class="per-pos">C</span>onstruction -->
+</div>
 </div>
 </div>
 <?php }?>

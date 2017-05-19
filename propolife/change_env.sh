4cf7deb45176a305c8d80cf42cef565ac057b5f0
Khanh Nguyen <khanh@fudosan-king.jp>
2017-05-19 09:39:32 +0700

    adjust something for aodaihousing-support page

--- propolife/change_env.sh
+++ propolife/change_env.sh
@@ -1,15 +1,17 @@
-if [[ $1 == 'golive' ]] ; then
-    echo '\n### ### [LYSBox - K Group] ### ###\n'
-    if [ -f "wp-config.golive.php" ] ; then
-        mv wp-config.php wp-config.dev.php
-        mv wp-config.golive.php wp-config.php
-        echo 'Config mode changed to GOLIVE MODE !!!\n'
-    else
-        echo 'Config GOLIVE MODE is running ...\n'
-    fi
-else
-    if [[ $1 == 'dev' ]]; then
-        echo '\n### ### [LYSBox - K Group] ### ###\n'
+echo '\n### ### [LYS Box - K Group] ### ###\n'
+echo 'Parameters Description:  change_env.sh [param]\n' ;
+
+case $1 in 
+    -golive)
+        if [ -f "wp-config.golive.php" ] ; then
+            mv wp-config.php wp-config.dev.php
+            mv wp-config.golive.php wp-config.php
+            echo 'Config mode changed to GOLIVE MODE !!!\n'
+        else
+            echo 'Config GOLIVE MODE is running ...\n'
+        fi
+    ;;
+    -dev)
         if [ -f "wp-config.dev.php" ] ; then
             mv wp-config.php wp-config.golive.php
             mv wp-config.dev.php wp-config.php
@@ -17,10 +19,9 @@ else
         else
             echo 'Config DEV MODE is running ...\n'
         fi
-    else
-        echo '\n### ### [LYSBox - K Group] ### ###\n'
-        echo 'Parameters Description:  config-change [param]\n' ;
-        echo '# golive: Change config file to life environment' ;
-        echo '# dev: Change config file to dev environment\n' ;
-    fi
-fi
+    ;;
+    *)
+        echo '# -golive: Change config file to life environment' ;
+        echo '# -dev: Change config file to dev environment\n' ;
+    ;;
+esac

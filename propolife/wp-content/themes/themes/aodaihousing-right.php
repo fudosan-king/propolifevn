2fcaff5e22a6c97d6e4380f4b7f74e6dc9718e39
Truong Tam <tam@fudosan-king.jp>
2017-06-16 10:32:55 +0700

    change images banner right

--- propolife/wp-content/themes/themes/aodaihousing-right.php
+++ propolife/wp-content/themes/themes/aodaihousing-right.php
@@ -1,10 +1,10 @@
 <div align="center" class="ct-right">
 	<ul>
 	    <li style="margin: 10px 0 10px 0;">
-	        <a target="_blank" class="link" href="http://aodaihousing.com/"><img style="width: 100%;" src="http://office-vn.com/static/uploads/bannersidebar/1406879833_banner-links.png"></a>
+	        <a target="_blank" class="link" href="http://aodaihousing.com/"><img style="width: 100%;" src="<?php bloginfo( 'template_directory' );?>/images/banner-links.png"></a>
 	    </li>
 	    <li style="margin: 10px 0 10px 0;">
-	        <a target="_blank" class="link" href="http://ameblo.jp/aozaihousing/"><img style="width: 100%;" src="http://office-vn.com/static/uploads/bannersidebar/1446783175_banner7.png"></a>
+	        <a target="_blank" class="link" href="http://ameblo.jp/aozaihousing/"><img style="width: 100%;" src="<?php bloginfo( 'template_directory' );?>/images/banner.png"></a>
 	    </li>
 
 	</ul>

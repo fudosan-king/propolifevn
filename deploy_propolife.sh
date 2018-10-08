da310746d1dc5e4aacbab09c54a2e695ac5c6347
Khanh Nguyen <khanh@fudosan-king.jp>
2018-10-08 16:44:41 +0700

    update propolifevn menu

--- deploy_propolife.sh
+++ deploy_propolife.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync --delete -e 'ssh -i root@222.255.217.23' -rlpcgz -v --exclude-from=excludes ./propolife root@222.255.217.23:/var/www/html
+rsync --delete -e 'ssh -i root@222.255.217.23' -rlpcgz -v --exclude-from=excludes ./propolife/wp-content/ root@222.255.217.23:/var/www/html/propolife/wp-content

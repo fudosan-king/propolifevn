e1c009b91bf813bb695d1fcdfb56cb0dc2d4ad35
Truong Tam <tam@fudosan-king.jp>
2019-07-10 10:56:09 +0700

    Update CG

--- cg_propolife/page-contact.php
+++ cg_propolife/page-contact.php
@@ -6,7 +6,6 @@
 		<div class="row">
 			<div class="col-12 col-md-10 mx-auto">
 			<h4><i class="i_contact"></i> Contact to Us</h4>
-			<div class="nf-form-fields-required">Fields marked with an <span class="ninja-forms-req-symbol">*</span> are required</div>
 			<div class="frm_contactus">
 				<?php echo do_shortcode('[ninja_form id=1]'); ?>
 			</div>

c3427af8c02a5bb5510e2da8d56b88937d8cc3f8
Khanh Nguyen PPL <khanh@fudosan-king.jp>
2019-09-16 14:21:49 +0700

    update propolifevn index

--- propolife/wp-content/themes/themes/sidebar-menu-aodai.php
+++ propolife/wp-content/themes/themes/sidebar-menu-aodai.php
@@ -12,7 +12,7 @@
 <a href="<?php echo get_permalink(264);?>" class="list-group-item"><span class="list_num">02.</span><?php echo get_the_title(11);?></a>
 <a href="/cat-chronicle/内装サービスについて" class="list-group-item"><span class="list_num">03.</span><?php echo get_the_title(73);?></a>
 <!-- <a href="<?php //echo get_permalink(370);?>" class="list-group-item"><span class="list_num">04.</span><?php //echo get_the_title(13);?></a> -->
-<a href="<?php echo get_permalink(get_page_by_path('aodaihousing-support'));?>" target="_blank" class="list-group-item"><span class="list_num">04.</span>不動産賃貸仲介</a>
+<a href="<?php echo get_permalink(get_page_by_path('aodaihousing-support'));?>" target="_blank" class="list-group-item"><span class="list_num">04.</span>不動産サービス</a>
 <a href="<?php echo get_permalink(get_page_by_path('contact'));?>" class="list-group-item"><span class="list_num">05.</span><?php echo get_the_title(1);?></a>
 </div>
 

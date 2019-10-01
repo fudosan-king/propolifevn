c07b68df904b6b46ca335569acafbc71b98368d1
NguyenDuyMinh <minh@fudosan-king.jp>
2019-10-01 14:22:40 +0700

    プロポライフ/ pls update menu info and any features

--- buying.aodaihousing/assets/js/contact.js
+++ buying.aodaihousing/assets/js/contact.js
@@ -17,44 +17,54 @@ $(document).ready(function(){
 
 		// Validate customer name
 		var validCustomerName = true;
+		$('#customer_name input[type=text]').removeClass('error-background');
 		if ($('input[name="customer_name"]').val() == ''){
 			$('#customer_name div.error-text').html('値を入力してください').fadeIn();
+			$('#customer_name input[type=text]').addClass('error-background');
 			validCustomerName = false;
 		}
 
 		// Validate address
-		var validAddress = true;	
+		var validAddress = true;
+		$('#address input[type=text]').removeClass('error-background');	
 		if ($('input[name="address"]').val() == ''){
 			$('#address div.error-text').html('値を入力してください').fadeIn();
+			$('#address input[type=text]').addClass('error-background');
 			validAddress = false;
 		}
 
 		// Validate number phone
 		var validPhone = true;
+		$('#phone input[type=tel]').removeClass('error-background');
 		var Pattern = /\(?([0-9]{2})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
 		//var Pattern = /^+91(7\d|8\d|9\d)\d{9}$/;
 		// pattern support (123) 456 7899, (123).456.7899, (123)-456-7899, 123-456-7899, 123 456 7899, 1234567899
 		var inputPhone = $.trim($('input[name="number_phone"]').val());
 		if (inputPhone == ''){
 			$('#phone div.error-text').html('値を入力してください').fadeIn();
+			$('#phone input[type=tel]').addClass('error-background');
 			validPhone = false;
 		} else {
 			validPhone = Pattern.test(inputPhone);
 			if(!validPhone){
 				$('#phone div.error-text').html('間違った電話番号の形式').fadeIn();
+				$('#phone input[type=tel]').addClass('error-background');
 			}
 		}
 
 		// Validate email
 		var validEmail = true;
-		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;	
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		$('#email input[type=email]').removeClass('error-background');	
 		if ($('input[name="email"]').val() == ''){
 			$('#email div.error-text').html('値を入力してください').fadeIn();
+			$('#email input[type=email]').addClass('error-background');
 			validEmail = false;
 		}else {
 			validEmail = emailReg.test($('input[name="email"]').val());
 			if(!validEmail){
 				$('#email div.error-text').html('間違ったEメールの形式').fadeIn();
+				$('#email input[type=email]').addClass('error-background');
 			}
 		}
 
@@ -63,7 +73,7 @@ $(document).ready(function(){
 		var validInvestment = $('input[name="inlineCheckbox1[]"]').is(':checked');
 		var validReal_residence = $('input[name="inlineCheckbox2[]"]').is(':checked');
 		if (validInvestment == true && validReal_residence == true){
-			$('#checkbox div.error-text').html('値を入力してください').fadeIn();
+			$('#checkbox div.error-text').html('2つの値を同時に選択することはできません').fadeIn();
 			validInvestmentcheck = false;
 		}
 
--- buying.aodaihousing/template-parts/content-page.php
+++ buying.aodaihousing/template-parts/content-page.php
@@ -2,14 +2,13 @@
 	<section class="content-page">
 		<div class="container">
 			<div class="row">
-				<div class="col col-12">
+				<div class="col col-12 aodaihousing-introcation">
 					<?php 
 					if(have_posts()):
 						while(have_posts()): the_post();
 							$thumnailImage = wp_get_attachment_image_url( get_post_thumbnail_id(), $size = 'large', $icon = false );
 							?>
-
-							<h2 class="sub_title"><?php the_title(); ?></h2>
+							<h2 class="title"><span><?php the_title(); ?></span></h2>
 							
 							<div id="main-content">
 								<p><?php the_content(); ?></p>

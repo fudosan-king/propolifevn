9681707007fb34104443d194a22e41505628fcb8
Khanh Nguyen <khanhnp0228@pm.me>
2021-04-19 15:38:17 +0700

    update pplvn

--- propolife/wp-content/themes/themes/aodaihousing-right.php
+++ propolife/wp-content/themes/themes/aodaihousing-right.php
@@ -13,7 +13,7 @@
 	        <a target="_blank" class="link" href="http://chronicle.propolifevietnam.com/"><img style="width: 100%;" src="<?php bloginfo( 'template_directory' );?>/images/img_chronicle.png"></a>
 	    </li>
 	    <li style="margin: 10px 0 10px 0;">
-	        <a target="_blank" class="link" href="https://buying.aodaihousing.com/"><img style="width: 100%;" src="<?php bloginfo( 'template_directory' );?>/images/banner_RealEstate.jpg"></a>
+	        <a target="_blank" class="link" href="https://buying.aodaihousing.com/"><img style="width: 100%;" src="<?php bloginfo( 'template_directory' );?>/images/BANNER_building_buy_sell_4x_20200312b.jpg"></a>
 	    </li>
 	    <!-- <li style="margin: 10px 0 10px 0;">
 	        <a target="_blank" class="link" href="https://www.propolifevietnam.com/recruitment-propolife.html/"><img style="width: 100%;" src="https://www.propolifevietnam.com/wp-content/uploads/2019/09/recruitment.png"></a>
